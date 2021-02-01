c97cf8ebafaea94ed4ce2ed9c4405e7beaf221ca
shnomad <54664079+shnomad@users.noreply.github.com>
2021-02-01 15:30:42 +0900

    Update qt-update.sh

--- qt/qt-update.sh
+++ qt/qt-update.sh
@@ -8,8 +8,21 @@ if [ ! -f "$BASE_DIRECTORY/.qt-lib-update" ]; then
      echo "library update"
      apt update
      apt -y dist-upgrade
-     apt-get -y build-dep qt5-qmake libqt5gui5 libqt5webengine-data libqt5webkit5
-     apt-get -y install libudev-dev libinput-dev libxcb-xinerama0-dev libxcb-xinerama0 gdbserver
+     apt-get -y build-dep install libudev-dev libinput-dev libts-dev libxcb-xinerama0-dev libxcb-xinerama0 gdbserver
+     apt-get -y install libbluetooth-dev bluez-tools        
      touch $BASE_DIRECTORY/.qt-lib-update
 fi
 
+if [ ! -f "/usr/lib/arm-linux-gnueabihf/libEGL.so.1.0.0_backup" ]; then
+     mv /usr/lib/arm-linux-gnueabihf/libEGL.so.1.0.0 /usr/lib/arm-linux-gnueabihf/libEGL.so.1.0.0_backup
+        ln -s /opt/vc/lib/lbbrcmEGL.so /usr/lib/arm-linux-gnueabihf/libEGL.so.1.0.0
+        #ln -s /opt/vc/lib/libEGL.so /usr/lib/arm-linux-gnueabihf/libEGL.so.1.0.0
+fi
+
+if [ ! -f "/usr/lib/arm-linux-gnueabihf/libGLESv2.so.2.0.0_backup" ]; then
+        mv /usr/lib/arm-linux-gnueabihf/libGLESv2.so.2.0.0 /usr/lib/arm-linux-gnueabihf/libGLESv2.so.2.0.0_backup
+        ln -s /opt/vc/lib/lbbrcmGLESv2.so /usr/lib/arm-linux-gnueabihf/libGLESv2.so.2.0.0
+        #ln -s /opt/vc/lib/libGLESv2.so /usr/lib/arm-linux-gnueabihf/libGLESv2.so.2.0.0
+fi
+
+exit 0
