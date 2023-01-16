10cab94d32527e868dcca1820f2e8fd5c5b3140d
shnomad <54664079+shnomad@users.noreply.github.com>
2023-01-16 13:38:42 +0900

    Update 2.download-src.sh

--- opencv/2.download-src.sh
+++ opencv/2.download-src.sh
@@ -4,8 +4,8 @@ OPENCV_VERSION=4.1.0
 set -ex
 
 mkdir -p $BASE_DIRECTORY/opencv && pushd $BASE_DIRECTORY/opencv
-wget -O "opencv-${OPENCV_VERSION}.tar.gz" "https://github.com/opencv/opencv/archive/${OPENCV_VERSION}.tar.gz" 
-wget -O "opencv_contrib-${OPENCV_VERSION}.tar.gz" "https://github.com/opencv/opencv_contrib/archive/${OPENCV_VERSION}.tar.gz"
-tar -xvf "opencv-${OPENCV_VERSION}.tar.gz"
-tar -xvf "opencv_contrib-${OPENCV_VERSION}.tar.gz"
+wget -O opencv.zip "https://github.com/opencv/opencv/archive/{OPENCV_VERSION}.zip"
+wget -O opencv_contrib.zip "https://github.com/opencv/opencv_contrib/archive/{OPENCV_VERSION}.zip"
+unzip "opencv-${OPENCV_VERSION}.zip"
+unzip "opencv_contrib-${OPENCV_VERSION}.zip"
 popd
